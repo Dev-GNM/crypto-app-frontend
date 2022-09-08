64668844d9e6c12bda268479ca3cc361723912b6
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 08:59:18 +0300

    Adding navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav id="nav-list">
+             <div className="logo">
+                <h1><span>Crypto</span> Base</h1>
+            </div>
+            <div className="nav-bar">
+            <li><a href="/">Home</a></li>
+            <li><a href=".new-post-form">Sign In</a></li>
+            <li><a href="#footer">Register</a></li> 
+            <li><a href="#footer">Market</a></li> 
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
