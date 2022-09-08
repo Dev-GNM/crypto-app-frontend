855b8b920076b126747877eba573eb64951bff3b
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 13:07:31 +0300

    fetching movie images

--- src/components/Tradable.js
+++ src/components/Tradable.js
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Tradable() {
     const [endPoint, setEndpoints] = useState("")
     const [container, setContainer] = useState([])
-  
+    const [finalPoint, setFinalPoint] = useState("")
 
-    fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', {
-        "method": 'GET',
-        "headers": {
-            'X-RapidAPI-Key': '68a65b4a39mshcf2679edfb6b35ap1eda53jsnfed9262c1f89',
-            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-        }
-    })
-        .then(response => {
-            console.log(response.json());
-        })
-        .then(data => {
-            setContainer(data)
-        })
+    useEffect(() => {
+        fetchMovies()
+    },[finalPoint])
 
-        .catch(err => {
-            console.error(err);
-        });
+    const fetchMovies = () => {
+
+        fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, {
+            "method": 'GET',
+            "headers": {
+                'X-RapidAPI-Key': '68a65b4a39mshcf2679edfb6b35ap1eda53jsnfed9262c1f89',
+                'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setContainer(data.d)
+            })
+
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
 
     const onChangeHandler = (e) => {
         setEndpoints(e.target.value)
     }
     const submitHandler = e => {
         e.preventDefault()
+        setFinalPoint(endPoint)
     }
 
     return (
@@ -37,6 +46,14 @@ function Tradable() {
                 <input type="text" value={endPoint} onChange={onChangeHandler} placeholder="search your favorite movie" />
                 <button type="submit">Search</button>
             </form>
+            {container.map((item) => {
+                return (
+                    <div>
+                        <img src={item.i.imageURL} alt="search"/>
+                    <p>{item.l}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
