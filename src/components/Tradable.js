4bac3824e7e4f7e5fb3b51fd96625d18331444d4
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 11:31:10 +0300

    adding search button

--- src/components/Tradable.js
+++ src/components/Tradable.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
-function Tradable() {
-    const options = {
-        method: 'GET',
-        headers: {
+function Tradable() { 
+    
+    fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', {
+        "method": 'GET',
+        "headers": {
             'X-RapidAPI-Key': '68a65b4a39mshcf2679edfb6b35ap1eda53jsnfed9262c1f89',
             'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
         }
-    };
-    
-    fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-  return (
-    <div>
-      <h1>Top rated movies</h1>
-      <button onClick={options}>Refresh List</button>
-    </div>
-  );
+    })
+        .then(response => {
+            console.log(response.json());
+        })
+              
+        .catch(err => {
+            console.error(err);
+        });
+
+       
+    return (
+        <div>
+            <h1>Top rated movies</h1>
+           <form>
+            <input type="text">
+                <button type="search"></button>
+            </input>
+           </form>
+        </div>
+    );
 }
 export default Tradable;
